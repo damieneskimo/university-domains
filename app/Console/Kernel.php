47f4eb988560e6022d5323949fb1b94e54fef3d5
Damien Jiang <damieneskimo@gmail.com>
2021-01-11 07:15:13 +1300

    delete unused command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('db:update-university-when-cache-expired')->everyMinute();
+
     }
 
     /**
