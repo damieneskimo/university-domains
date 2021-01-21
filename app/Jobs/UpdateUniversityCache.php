f40e836570068ae9f40ca3984073b24d49ee8743
Damien Jiang <damieneskimo@gmail.com>
2021-01-22 08:21:06 +1300

    use laravel echo for broadcasting event to update single university

--- app/Jobs/UpdateUniversityCache.php
+++ app/Jobs/UpdateUniversityCache.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Events\UniversityCacheExpired;
 use App\Models\University;
 use GuzzleHttp\Client;
 use Illuminate\Bus\Queueable;
@@ -34,30 +35,6 @@ class UpdateUniversityCache implements ShouldQueue
      */
     public function handle()
     {
-        try {
-            $client = new Client;
-            $uri = University::SOURCE_API . 'name=' . $this->university->name . '&country=' . $this->university->country;
-            $res = $client->request('get', $uri);
-            $data = json_decode($res->getBody()->getContents());
-
-            if (is_array($data) && ! empty($data)) {
-                $record = $data[0];
-                $this->university->update([
-                    'alpha_two_code' => $record->alpha_two_code,
-                    'country' => $record->country,
-                    'state_province' => $record->{'state-province'},
-                    'name' => $record->name,
-                    'domains' => $record->domains,
-                    'ttl' => rand(5, 15)
-                ]);
-            } else {
-                // remove from local database if record not exists any more in source api data
-                $this->university->delete();
-            }
-        } catch (\Throwable $th) {
-            return response()->json([
-                'message' => 'Whoops! Something went wrong when retrieving data!'
-            ], 503);
-        }
+        UniversityCacheExpired::dispatch($this->university);
     }
 }
